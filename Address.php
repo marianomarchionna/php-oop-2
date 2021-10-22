3cf7049411054d45c7e3b29beb21ac7a38b9bf2b
Mariano <marianomarchionna8@gmail.com>
2021-10-22 15:25:49 +0200

    Creazione trait Address

---
+++ Address.php
@@ -0,0 +1,13 @@
+<?php
+
+    trait Address {
+        public $address;
+        public $city;
+        public $cap;
+
+        public function getAddress(){
+            return $this->address . ', ' . $this->city  . ' - ' . $this->cap;
+        }
+    }
+
+?>
---
+++ index.php
@@ -0,0 +1,9 @@
+<!-- Shop online. Ad esempio, ci saranno sicuramente dei prodotti da acquistare e degli utenti che fanno shopping.
+Strutturare le classi gestendo l'ereditarietà dove necessario.
+Ci potrebbero essere degli utenti premium che hanno diritto a degli sconti esclusivi,
+oppure diverse tipologie di prodotti.
+BONUS:
+Provate a far interagire tra di loro gli oggetti: ad esempio, l'utente dello shop inserisce una carta di credito...
+        $c = new CreditCard(..);
+        $user->insertCreditCard($c);
+2.    Gestite eventuali eccezioni che si possono verificare (es: carta di credito scaduta). -->
